c9e637c5069bd543ed9c2ea72cd02e43fff15be2
jgomez002 <jdg8858@nyu.edu>
2022-09-15 09:25:29 -0400

    update

---
+++ mockjs.js
@@ -0,0 +1,32 @@
+// string
+var cool = "hi";
+//
+let morecool = 123;
+//array
+const anotherCool = [];
+
+function coolFunction(){
+    return false;
+}
+
+const coolArrowFunction = () => false;
+
+const coolObject = {
+    hi: 'hello',
+    coolNumber: 123,
+    coolArray: []
+}
+
+function hi(){
+    return coolObject.coolNumber
+}
+
+const coolJson= {
+    {
+        hi: 'hi',
+    },
+    {
+        hi: 'hello',
+    },
+    coolObject
+}
